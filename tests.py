428a8f36cf9bbaced4905b9e6c318793a3ca591e
ccareaga <chris_careaga@sfu.ca>
2023-06-24 10:12:04 -0700

    testing msg loss

--- tests.py
+++ tests.py
@@ -11,16 +11,11 @@ test = torch.from_numpy(test)
 
 pred = test.permute(2, 0, 1).unsqueeze(0).repeat(8, 1, 1, 1)
 grnd = torch.ones((8, 3, 384, 384))
-mask = (torch.ones((1, 1, 384, 384))).float()
+mask = (torch.ones((8, 1, 384, 384))).float()
 
 msg_loss = MSGLoss(scales=4, taps=[2, 1, 1, 1], k_size=[3, 5, 7, 9])
 
-times = []
-for i in range(50):
-    start = timer()
-    loss = msg_loss(pred, grnd, mask)
-    duration = timer() - start
-    times.append(duration)
+loss_8 = msg_loss(pred, grnd, mask)
+loss_1 = msg_loss(pred[0].unsqueeze(0), grnd[0].unsqueeze(0), mask[0].unsqueeze(0))
+print(loss_1, loss_8)
 
-print(sum(times) / len(times), 'seconds on avg.')
-print(loss)
