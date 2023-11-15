db7e6e2bab1d76bf06cdcd0dace8e959fa44d966
Chris Careaga <chris_careaga@sfu.ca>
2023-11-14 16:13:46 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,20 +10,20 @@ setuptools.setup(
     license="",
     python_requires=">3.6",
     install_requires=[
-        beautifulsoup4==4.12.2
-        gdown==4.7.1
-        huggingface-hub==0.16.4
-        imageio==2.31.3
-        kornia==0.7.0
-        matplotlib==3.7.2
-        numpy==1.24.4
-        opencv-python==4.8.0.76
-        Pillow==10.0.0
-        requests==2.31.0
-        scikit-image==0.21.0
-        scipy==1.10.1
-        torch==2.0.1
-        torchvision==0.15.2
-        tqdm==4.66.1
+        'beautifulsoup4>=4.12.2',
+        'gdown>=4.7.1',
+        'huggingface-hub>=0.16.4',
+        'imageio>=2.31.3',
+        'kornia>=0.7.0',
+        'matplotli>=3.7.2',
+        'numpy>=1.24.4',
+        'opencv-python>=4.8.0.76',
+        'Pillow>=10.0.0',
+        'requests>=2.31.0',
+        'scikit-image>=0.21.0',
+        'scip>=1.10.1',
+        'torch>=2.0.1',
+        'torchvision>=0.15.2',
+        'tqdm>=4.66.1',
     ]
 )
